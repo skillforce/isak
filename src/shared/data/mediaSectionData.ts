9b6655d567fab8539a4bfca18b1279f771d51e0c
denis_tatarinov <skillforce@mail.ru>
2025-03-04 18:41:02 +0300

    + refactoring

---
+++ src/shared/data/mediaSectionData.ts
@@ -0,0 +1,9 @@
+export const MEDIA_SECTION_DATA = {
+  VIDEOS_ID: [
+    "wwzq9kUryF8?si=X_kpOn4CDefmvvKH",
+    "AczPuWS2BeQ?si=29lXFXkqh68Hu0so",
+  ],
+  BUTTON_TITLE: "Заказать",
+  COMPANY_NAME: "Gera2 ©",
+  LOCATION_TIME: "Belarus, Mogilev 2025",
+};
