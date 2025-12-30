c3dae01782008b5efad6ab28219f62fce190a11f
denis_tatarinov <skillforce@mail.ru>
2025-12-30 18:12:56 +0300

    with new design + option feature

--- src/shared/data/modal_data.ts
+++ src/shared/data/modal_data.ts
@@ -3,25 +3,49 @@ import viberIcon from "../../assets/contact_icons/viber.svg";
 import phoneIcon from "../../assets/contact_icons/phone.svg";
 import whatsUpIcon from "../../assets/contact_icons/watsup.svg";
 
-export const MODAL_DATA = {
+
+export type ContactType = "whatsapp" | "viber" | "telegram" | "phone";
+
+export interface Contact {
+  type: ContactType;
+  href: string;
+  icon: typeof whatsUpIcon; // or ImageMetadata if using Astro's image type
+  label: string;
+}
+
+export interface ModalData {
+  CONTACT_MODAL_ID: string;
+  sticker_price_text: string;
+  CONTACTS: Contact[];
+}
+
+export const MODAL_DATA:ModalData = {
   CONTACT_MODAL_ID: "contact_modal",
   sticker_price_text: "Цена 150BYN",
   CONTACTS: [
     {
-      href: "https://wa.me/+375296083656",
+      type: "whatsapp",
+      href: "https://wa.me/375296083656",
       icon: whatsUpIcon,
       label: "WhatsApp",
     },
     {
-      href: "viber://chat?number=+375296083656",
+      type: "viber",
+      href: "viber://chat?number=%2B375296083656",
       icon: viberIcon,
       label: "Viber",
     },
     {
-      href: "https://t.me/+375296083656",
+      type: "telegram",
+      href: "tg://resolve?domain=+375447003309",
       icon: telegramIcon,
       label: "Telegram",
     },
-    { href: "tel:+375296083656", icon: phoneIcon, label: "Позвонить" },
+    {
+      type: "phone",
+      href: "tel:+375296083656",
+      icon: phoneIcon,
+      label: "Позвонить",
+    },
   ],
 };
